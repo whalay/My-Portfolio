4a97cc578e1b3fe630365153f50fd7e540c042cd
soliu yusuf <soliuy5@gmail.com>
2022-07-24 15:30:51 +0100

    Refactored

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
       <form method='POST' action="https://getform.io/f/789efd1d-e00b-4f58-9c57-9c3ec9da8ec5" className='flex flex-col max-w-[600px] w-full ' >
         <div className='py-10 mt-16'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-          <p className=' text-gray-300 p-4'>/ Submit the form below or shoot me an email - soliuy20@gmail.com </p>
+          <p className=' text-gray-300 py-4'>/ Submit the form below or shoot me an email - soliuy20@gmail.com </p>
         </div>
 
         <input className=' p-2 bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
