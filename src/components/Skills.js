11a0de0fec5f2610ebef6abdf9bc19c862324da2
soliu yusuf <soliuy5@gmail.com>
2022-08-19 18:51:29 +0100

    skills edited

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -53,43 +53,22 @@ const Skills = () => {
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
           <p className='py-2'>/These are the technology i have learnt so far</p>
         </div>
-        
+
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gaps-4 text-center py-8'>
           {Skill.map((ski, i) => {
             return (
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration--500 mt-4'>
-                  <img className='w-20 mx-auto' src={ski.image} alt="" />
-                  <p className='my-4'>{ski.name}</p>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration--500 mt-4'>
+                <img className='w-20 mx-auto' src={ski.image} alt="" />
+                <p className='my-4'>{ski.name}</p>
 
-                </div>
-             
+              </div>
+
             )
           })}
 
         </div>
 
-        {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration--500 mt-4'>
-            <img className='w-20 mx-auto' src={react} alt="" />
-            <p className='my-4'>REACT</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration--500 mt-4'>
-            <img className='w-20 mx-auto' src={javascript} alt="" />
-            <p className='my-4'>JAVASCRIPT</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration--500 mt-4'>
-            <img className='w-20 mx-auto' src={css} alt="" />
-            <p className='my-4'>CSS</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration--500 mt-4'>
-            <img className='w-20 mx-auto' src={github} alt="" />
-            <p className='my-4'>GITHUB</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration--500 mt-4'>
-            <img className='w-20 mx-auto' src={node} alt="" />
-            <p className='my-4'>NODE</p>
-          </div>
 
-        </div> */}
 
       </div>
     </div>
