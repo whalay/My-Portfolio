593953830cd8d96de9986c99a6bcf7a2004d6657
Whalay <soliuy5@gmail.com>
2022-12-18 20:59:52 +0100

    home fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,11 +11,11 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full'>
                 <p className='text-pink-600 '>Hi, My name is</p>
                 <h1 className='text-4xl font-bold sm:text-7xl text-gray-300 '>Soliu Yusuf</h1>
-                <h2 className='text-4xl sm:7xl font-bold text-gray-500'>I'm a Full Stack Developer
+                <h2 className='text-4xl sm:7xl font-bold text-gray-500'>I'm a Frontend Developer
                 </h2>
-                <p className='text-gray-200 py-4 max-w-[700px]'>I'm a full-stack developer specilizing in building and occasionally
+                <p className='text-gray-200 py-4 max-w-[700px]'>I'm a frontend developer specilizing in building and occasionally
                     designing exceptional digital experiences. Currently I'm focused on
-                    building responsive Full-tack web applications.
+                    building responsive web applications.
                 </p>
                 <div>
                     <Link to="work">
